78f9deab3e8a77d07957fc9c1abafc2d7bb17714
GreenFrog53 <james.wagland@students.mq.edu.au>
2024-06-29 20:46:14 +1000

    reworked code and added flickity support. Barcode will now refresh when url changes are detected

---
+++ background.js
@@ -0,0 +1,6 @@
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.url) {
+      chrome.tabs.sendMessage(tabId, { url: changeInfo.url });
+    }
+  });
+  
--- content.js
+++ content.js
@@ -1,13 +1,37 @@
-
 // Select the element with the specific class and ID
 const pluElement = document.querySelector('.zm2jk14#pdp-title-plu');
 
+
+
 if (pluElement) {
+  
+  // Detects when the page URL has changed (due to carousel involvement and updates the PLU Barcode). Listens to message from background.js
+  chrome.runtime.onMessage.addListener((message) => {
+    
+    if (message.url) {
+      
+      console.log("Page Change detected");
+      
+      refreshBarcode();
+
+    }
+  });
+
+  // First run generates barcode
+  refreshBarcode();
+  
+} 
+else {
+  console.log("PLU element not found on this webpage.");
+}
+
+// Function for the code that actually refreshes the barcode on the page
+function refreshBarcode() {
   
   // Extract the current PLU number (assuming it's after the colon)
   const currentPlu = pluElement.textContent.split(':')[1].trim();
-  // Print the original PLU to the console
-  console.log("Original PLU:", currentPlu);
+  // Print the PLU to the console
+  console.log("PLU:", currentPlu);
   
   const baseUrl = 'http://bwipjs-api.metafloor.com/?bcid=code128&text='
 
@@ -16,11 +40,10 @@ if (pluElement) {
   image.src = baseUrl + currentPlu;
 
   // Replace the element content with the image
-  pluElement.textContent = ""; // Clear existing plu
-  pluElement.appendChild(image);
-
-} else {
-  console.log("PLU element not found on this webpage.");
+  pluElement.insertBefore(image, pluElement.firstChild);
+  
+  console.log("The barcode has been refreshed.");
+  return null;
 }
 
 // Made with love xx
